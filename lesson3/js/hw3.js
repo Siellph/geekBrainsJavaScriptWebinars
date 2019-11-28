ebddad2a29a339935450e13d05706d0da49f162c
Vladislav <vl241095@gmail.com>
2019-11-29 00:38:58 +0500

    что-то я в тупике

---
+++ lesson3/js/hw3.js
@@ -0,0 +1,18 @@
+// Задание 1
+function unit1 () {
+    let i = 0
+    let arr = []
+    while (arr.lenght <= 100) {
+
+    }
+}
+
+function unit4One () {
+let arr = []
+for (let i = 0; i <= 9; arr.push (i++)) {}
+console.log (arr)
+}
+
+function unit4Two () {
+
+}
