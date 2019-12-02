0d5c71ece3ae272bd11b8c074d087729bc99e0c4
Vladislav <vl241095@gmail.com>
2019-12-03 03:04:17 +0500

    homeWork3

--- lesson3/js/hw3.js
+++ lesson3/js/hw3.js
@@ -1,18 +1,37 @@
 // Задание 1
-function unit1 () {
-    let i = 0
+function getSimples (limit) {
+    let arr = [2]
+    let num = 2
+    
+            while (num <= limit) {
+                if (checkNum(num, arr)) arr.push (num)
+                ++num
+            }
+            console.log (arr)
+        }
+    
+        function checkNum (number, arrr) {
+            let check = true
+            arrr.forEach (el => {
+                if (number % el == 0) {
+                    check = false
+                }
+            })
+            return check
+        }
+// Задание 4.1
+function unit4One() {
     let arr = []
-    while (arr.lenght <= 100) {
-
-    }
+    for (let i = 0; i <= 9; arr.push(i++)) {}
+    console.log(arr)
 }
-
-function unit4One () {
+// Задание 4.2
+let symb = '*'
+let v = +prompt('Высота пирамиды:')
 let arr = []
-for (let i = 0; i <= 9; arr.push (i++)) {}
-console.log (arr)
-}
-
-function unit4Two () {
-
+function piramid() {
+    for (let i = 0; i < v; i++) {
+        arr.push (symb)
+        console.log(arr)
+    }
 }
