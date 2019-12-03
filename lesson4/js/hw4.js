05e31882b9cc6e6be60de2ccf9282a00c1080fc1
Vladislav <vl241095@gmail.com>
2019-12-04 01:10:19 +0500

    Update hw4.js

--- lesson4/js/hw4.js
+++ lesson4/js/hw4.js
@@ -1,6 +1,42 @@
-let vova = {
-    name: 'Vladimir',
-    goWork: function() {
-        console.log ('Vova goes work')
+//Задание 1
+function preNum(a) {
+    if (a > 999) {
+        console.log('Ожидалось трехзначное число')
+    } else {
+        let num = {
+            единицы: Math.floor((a / 1) % 10),
+            десятки: Math.floor((a / 10) % 10),
+            сотни: Math.floor((a / 100) % 10)
+        }
+        console.log(num)
     }
+}
+//Задание 2
+let PRODUCTS_NAMES = ['Processor', 'Display', 'Notebook', 'Mouse', 'Keyboard']
+let PRICES = [100, 120, 1000, 15, 18]
+let IDS = [0, 1, 2, 3, 4]
+
+let products = [] //массив объектов
+let cart = {
+    items: [],
+    total: 0
+}
+
+function getData() {
+    for (let i = 0; i < IDS.length; i++) {
+        products.push(createNewPrduct(i))
+    }
+}
+
+function createNewProduct(index) {
+    return {
+        product_name: PRODUCTS_NAMES[index],
+        price: PRICES[index],
+        product_id: [index]
+    }
+}
+
+function addProduct() {
+    let find = products.find(el.product_id === id)
+    cart.items.push(Object.assign({}, find, {quantity: 1}))
 }
